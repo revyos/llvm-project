799e4300d773f1d8a00a2aaa28c738101a41301e
Han Gao <gaohan@iscas.ac.cn>
2024-08-06 23:24:30 +0800

    [Not-for-upstream] [Riscv] [THead]: Use xthead extension
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- clang/lib/Driver/ToolChains/Arch/RISCV.cpp
+++ clang/lib/Driver/ToolChains/Arch/RISCV.cpp
@@ -302,7 +302,8 @@ StringRef riscv::getRISCVArch(const llvm::opt::ArgList &Args,
       if (Triple.isAndroid())
         return "rv64imafdcv_zba_zbb_zbs";
 
-      return "rv64imafdc";
+      // return "rv64imafdc";
+      return "rv64gc_zfh_xtheadba_xtheadbb_xtheadbs_xtheadcmo_xtheadcondmov_xtheadfmemidx_xtheadmac_xtheadmemidx_xtheadmempair_xtheadsync";
     }
   }
 
@@ -322,7 +323,8 @@ StringRef riscv::getRISCVArch(const llvm::opt::ArgList &Args,
     else if (Triple.isAndroid())
       return "rv64imafdcv_zba_zbb_zbs";
     else
-      return "rv64imafdc";
+//      return "rv64imafdc";
+      return "rv64gc_zfh_xtheadba_xtheadbb_xtheadbs_xtheadcmo_xtheadcondmov_xtheadfmemidx_xtheadmac_xtheadmemidx_xtheadmempair_xtheadsync";
   }
 }
 
